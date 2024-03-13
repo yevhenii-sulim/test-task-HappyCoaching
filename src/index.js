075cf47b793e4831635f210b1bc1f6ac65892f7d
evgeniy210191 <bees127@ukr.net>
2024-03-13 22:34:08 +0200

    made middle one

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const buttonMenu = document.querySelector('.nav-button')
 const navMenu = document.querySelector('.nav-menu')
 const backdrop = document.querySelector('.backdrop')
-console.log(backdrop,navMenu)
 buttonMenu.addEventListener('click', toggleMenu)
 function toggleMenu (){
 	const buttonMenuTitle = document.querySelectorAll('.button-menu')
@@ -10,4 +9,23 @@ function toggleMenu (){
 	}
 	navMenu.classList.toggle('hidden')
 	backdrop.classList.toggle('hidden')
+}
+
+const questionList = document.querySelector(".question-list")
+questionList.addEventListener('click', toggleQuestionText)
+
+function toggleQuestionText(evt){
+	const question = evt.target.closest('li');
+	const questionSignOpenClose = question.querySelector('.open-close');
+	const textQuestion = question.querySelector(".text-question")
+	console.log( questionSignOpenClose)
+	if(textQuestion.classList.contains("hidden")){
+		console.log('open')
+		textQuestion.classList.remove('hidden');
+		questionSignOpenClose.style = `background-position: -11px -98px`
+	} else {
+		console.log('close')
+		textQuestion.classList.add('hidden');
+		questionSignOpenClose.style = `background-position: -55px -96px`
+	}
 }
