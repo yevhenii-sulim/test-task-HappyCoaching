65962f913f7c87b915b811ad4910bf154c9948ba
yevhenii-sulim <88433785+yevhenii-sulim@users.noreply.github.com>
2024-03-13 08:19:11 +0000

    Deploying to gh-pages from @ yevhenii-sulim/test-task-HappyCoaching@b9bf6ea1547392156745e29043f4d6901d8d4943 🚀

---
+++ index.ad197a6b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".nav-button"),t=document.querySelector(".nav-menu"),o=document.querySelector(".backdrop");console.log(o,t),e.addEventListener("click",(function(){const e=document.querySelectorAll(".button-menu");for(const t of e)t.classList.toggle("hidden");t.classList.toggle("hidden"),o.classList.toggle("hidden")}));
+//# sourceMappingURL=index.ad197a6b.js.map
